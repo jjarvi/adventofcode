a1f8d350e67c6843ccdec7415299fe9b63bf45aa
Jari Jarvi <jari.jarvi@gmail.com>
2019-04-08 21:22:39 +0300

    Day 8 solution

---
+++ src/day08.cpp
@@ -0,0 +1,118 @@
+#include <gtest/gtest.h>
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+class Parser
+{
+public:
+    Parser(const std::vector<int>& input) :
+        input(input),
+        index(0),
+        sum(0)
+    {
+        rootValue = parse();
+    }
+
+    int parse()
+    {
+        int nChildren = input[index++];
+        int nMetadata = input[index++];
+        int value = 0;
+        std::vector<int> childValues;
+
+        if (nChildren > 0)
+        {
+            for (int i = 0; i < nChildren; ++i)
+            {   
+                childValues.push_back(parse());
+            }
+        }
+
+        for (int i = 0; i < nMetadata; i++)
+        {
+            int m = input[index++];
+            sum += m;
+            value += getNodeValueIncrement(m, childValues);
+        }
+
+        return value;
+    }
+
+    int getNodeValueIncrement(int metadata, const std::vector<int>& childValues) const
+    {
+        if (childValues.size() == 0)
+        {
+            return metadata;
+        }
+        else if (metadata > 0 && metadata <= static_cast<int>(childValues.size()))
+        {
+            return childValues[metadata - 1];
+        }
+        return 0;
+    }
+
+    int getSumOfMetadata() const
+    {
+        return sum;
+    }
+
+    int getRootValue() const
+    {
+        return rootValue;
+    }
+
+private:
+    const std::vector<int>& input;
+    int index;
+    int sum;
+    int rootValue;
+};
+
+
+TEST(Day08, metadataOnly)
+{
+    Parser p({0, 2, 10, 11});
+    EXPECT_EQ(21, p.getSumOfMetadata());
+}
+
+TEST(Day08, oneChild)
+{
+    Parser p({1, 2, 0, 1, 2, 10, 11});
+    EXPECT_EQ(23, p.getSumOfMetadata());
+}
+
+TEST(Day08, twoChildren)
+{
+    Parser p({2, 2, 0, 1, 5, 0, 0, 10, 11});
+    EXPECT_EQ(26, p.getSumOfMetadata());
+}
+
+TEST(Day08, example)
+{
+    Parser p({2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2});
+    EXPECT_EQ(138, p.getSumOfMetadata());
+    EXPECT_EQ(66, p.getRootValue());
+}
+
+TEST(Day08, solution)
+{
+    std::vector<int> tree;
+    std::ifstream input("../day08_input.txt");
+    std::string line;
+    while (std::getline(input, line))
+    {
+        std::string num;
+        std::stringstream ss(line);
+        while (std::getline(ss, num, ' '))
+        {
+            tree.push_back(std::stoi(num));
+        }
+    }
+
+    Parser p(tree);
+    EXPECT_EQ(48260, p.getSumOfMetadata());
+    EXPECT_EQ(25981, p.getRootValue());
+}
